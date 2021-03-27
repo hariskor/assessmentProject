0af1f5b83321ed766835df582412ff435ad1090e
Haris <haris.kormenidis@yachtwizard.eu>
2021-03-27 00:24:59 +0000

    payment mvcs initial setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ClientSeeder::class
+            ClientSeeder::class,
+            PaymentSeeder::class
         ]);
 
         // \App\Models\User::factory(10)->create();
