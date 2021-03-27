c130677b1d3912ef6e2b08ebd7a2f3201fefa085
Haris <haris.kormenidis@yachtwizard.eu>
2021-03-27 23:44:38 +0000

    route for the endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return redirect('/client');
 });
+//Route::get('/client', 'ClientController@index');
+
+Route::get('/client',[ClientController::class,'index']);
+
